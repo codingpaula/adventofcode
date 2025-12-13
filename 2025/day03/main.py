4fb9b7714ca7a9fbd9f670f03f17139772fca60f
Paula Kammler <paula@zauberzeug.com>
2025-12-13 17:14:50 +0100

    day03: add part two

--- 2025/day03/main.py
+++ 2025/day03/main.py
@@ -4,8 +4,26 @@ with open('./input.txt') as input:
 
 banks = [line.strip() for line in lines]
 result = 0
+result_two = 0
 
 for bank in banks:
+    def find_highest(left_bound: int, right_bound: int, jolts: str) -> int:
+        i = left_bound + 1
+        while i < len(jolts) - right_bound:
+            if jolts[i] > jolts[left_bound]:
+                left_bound = i
+            i = i + 1
+        return left_bound
+
+    rounds = 11
+    digits = ''
+    left = 0
+    while rounds >= 0:
+        left = find_highest(left, rounds, bank)
+        digits = digits + bank[left]
+        rounds = rounds - 1
+        left = left + 1
+
     first_i = 0
     secon_i = 0
     i = 1
@@ -23,6 +41,8 @@ for bank in banks:
         i = i + 1
 
     result = result + int(bank[first_i] + bank[secon_i])
+    result_two = result_two + int(digits)
 
 
 print('part one:', result)
+print('part two:', result_two)
