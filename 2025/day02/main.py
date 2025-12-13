442dd421db3d65d8a2e6552c9c57c355c28ae5b6
Paula Kammler <paula@kammler.co>
2025-12-13 15:36:02 +0100

    day02: solve part one

--- 2025/day02/main.py
+++ 2025/day02/main.py
@@ -7,7 +7,7 @@ with open('./input.txt') as input:
 raws: list[str] = [line.strip() for line in lines]
 print(raws)
 
-ranges: list[tuple[str, str]] = []
+ranges: list[tuple[int, int]] = []
 
 for r in raws:
     x_and_ys = r.split(',')
@@ -15,9 +15,23 @@ for r in raws:
         if len(x_y) > 2:
             x, y = x_y.split('-')
             if x and y:
-                ranges.append((x, y))
+                ranges.append((int(x), int(y)))
 
 
+summe = 0
 for ids in ranges:
     first, last = ids
+    print(f'checking from {first} to {last}')
+    while first <= last:
+        length = len(str(first))
+        if length % 2 == 0:
+            exponent = 10**(length / 2)
+            last_digits = first % exponent
+            first_digits = (first - last_digits) / exponent
+            if last_digits == first_digits:
+                print('found one:', first)
+                summe = summe + first
+        first = first + 1
+
 print(ranges)
+print(summe)
