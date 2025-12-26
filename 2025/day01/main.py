592eda9b870c75fd73b32d4d6f9513ab03206ee3
Paula Kammler <paula@zauberzeug.com>
2025-12-26 01:11:09 +0100

    day01: fix part two and simplify code

--- 2025/day01/main.py
+++ 2025/day01/main.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
-import math
 
 with open('./input.txt') as input:
     lines = input.readlines()
 
 commands = [line.strip() for line in lines]
-# commands = ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"]
 
 current = 50
 result = 0
@@ -15,32 +13,38 @@ part_two = 0
 for command in commands:
     amount = int(command[1:])
 
-    new_full_current = (current - amount) \
+    while amount > 0:
+        if command[0] == 'L':
+            current = current - 1
+            if current == -1:
+                current = 99
+        else:
+            current = current + 1
+            if current == 100:
+                current = 0
+
+        if current == 0:
+            part_two = part_two + 1
+        amount = amount - 1
+
+print('part_two', part_two)
+
+
+current = 50
+for command in commands:
+    amount = int(command[1:])
+    new_current = (current - amount) \
         if command[0] == 'L' else (current + amount)
 
-    new_current =
-
     if new_current > 99:
-        if current == 0 or new_current % 100 == 0:
-            rounds = math.floor(new_current / 100) - 1
-        else:
-            rounds = math.floor(new_current / 100)
         current = new_current % 100
-        part_two = part_two + rounds
-        print(current, ':', new_current, '> 99: +', rounds)
     elif new_current < 0:
-        if current == 0 or new_current % 100 == 0:
-            rounds = math.floor(new_current / 100) + 1
-        else:
-            rounds = math.floor(new_current / 100)
         current = (100 + new_current) % 100
-        part_two = part_two - rounds
-        print(current, ':', new_current, '< 0: +', -rounds)
     else:
         current = new_current
 
     if current == 0:
         result = result + 1
 
-print(result)
-print(result+part_two)
+
+print('part_one', result)
